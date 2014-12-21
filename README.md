e4760bd358e642c1fb6a66f701c279666aabc100
Nathan Typanski <ntypanski@gmail.com>
2014-12-21 18:16:22 -0500

    readme updates

--- README.md
+++ README.md
@@ -1,3 +1,76 @@
 # cabver.py
 
 Quick script to make parsing the output of [cabal](https://www.haskell.org/cabal/) and [ghc-pkg](https://www.haskell.org/haskellwiki/Ghc-pkg) a bit easier.
+
+## Usage
+
+To show installed packages:
+
+```
+$ ./cabver.py -i
+Cabal 1.18.1.3 1.20.0.3
+Decimal 0.4.1
+HTTP 4000.2.17 4000.2.19
+JuicyPixels 3.1.7.1
+List 0.5.1
+QuickCheck 2.7.6
+SHA 1.6.4.1
+X11 1.6.1.2
+aeson 0.7.0.6
+aeson-pretty 0.7.1
+array 0.5.0.0
+asn1-encoding 0.8.1.3
+...
+warp 3.0.0.8
+x509 1.4.12
+x509-store 1.4.4
+x509-system 1.4.5
+x509-validation 1.5.0
+xhtml 3000.2.1
+xml 1.3.13
+yaml 0.8.8.4
+zip-archive 0.2.3.2 0.2.3.4
+zlib 0.5.4.1
+```
+
+You can also display just a specific package:
+
+```
+./cabver.py -i pandoc
+pandoc 1.13.1
+```
+
+Or a list of them:
+
+```
+./cabver.py -i pandoc unix
+pandoc 1.13.1
+unix 2.7.0.1
+```
+
+Passing `--old` will list only "old" packages, i.e., packages for which a newer version is currently registered.
+
+```
+./cabver.py -i --old
+Cabal 1.18.1.3
+HTTP 4000.2.17
+network 2.4.2.3
+texmath 0.6.6.3
+zip-archive 0.2.3.2
+```
+
+This may be combined with the other switches as needed:
+
+```
+$ ./cabver.py -i zip-archive Cabal --old
+Cabal 1.18.1.3
+zip-archive 0.2.3.2
+```
+
+Providing the `--id` argument gives nice IDs for use with `ghc-pkg` or `cabal-install`:
+
+```
+$ ./cabver.py -i zip-archive Cabal --old --id
+Cabal-1.18.1.3
+zip-archive-0.2.3.2
+```
--- cabver.py
+++ cabver.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 
-import subprocess
 import argparse
-import collections
 from distutils.version import LooseVersion
-import re
+import collections
+import subprocess
 import sys
+
 
 def build_args():
     args = argparse.ArgumentParser()
-    args.add_argument('-l', '--list', metavar='NAME', nargs='*')
+    args.add_argument('-i', '--installed',
+                      help='list installed versions',
+                      type=str,
+                      nargs='*')
+    args.add_argument('-o', '--old',
+                      help='Operate only on old versions of packages.'
+                           ' for example, when used with --installed,'
+                           ' this will list only old versions of installed'
+                           ' packages.' ,
+                      action='store_true', default=False)
     display = args.add_argument_group('display options')
     display.add_argument('--version-separator',
                          help='separate version numbers with SEP.'
@@ -20,37 +29,61 @@ def build_args():
                          help='display a ghc-pkg compatible id'
                               ' for each package.'
                               ' e.g., Cabal-1.20.0.3',
-                         action='store_true')
+                         action='store_true', default=False)
     return args
 
+
 def get_package_list():
-    pkg = subprocess.Popen(['ghc-pkg', 'list', '--user'], stdout=subprocess.PIPE, universal_newlines=True)
-    stdout, stderr = pkg.communicate()
-    packages = collections.defaultdict(list)
-    lines = [line.strip() for line in stdout.split('\n') if line.startswith(' ')]
-    for package in lines:
-        package_parser = re.compile('([a-zA-Z][a-zA-Z-\d]*)-(\d+(\.\d+)+)')
-        match = package_parser.match(package)
-        if match:
-            name = match.group(1)
-            ver = match.group(2)
-            packages[name].append(LooseVersion(ver))
+    """Get a list of installed packages and their versions from Cabal.
+
+    The output will be a list of the form [[package, version]].
+    """
+    cabal_args = ['cabal', 'list', '--installed', '--simple-output']
+    cabal = subprocess.check_output(cabal_args, universal_newlines=True)
+
+    package_list = [pkg.split(' ') for pkg in cabal.strip('\n').split('\n')]
+    package_map = collections.defaultdict(list)
+    for name, version in package_list:
+        package_map[name].append(version)
+    package_list = [(name, versions) for name, versions in package_map.items()]
+    package_list.sort(key=lambda x: x[0])
+    return package_list
+
+
+def display_packages(packages, version_separator=None, id_form=False):
+    """Prints to STDOUT a list of package names and their versions.
+
+    Args:
+        packages: a list [name, [versions]], where versions will be converted
+            into strings before printing, and name is a single string.
+    """
+    for name, versions in packages:
+        if id_form:
+            for ver in versions:
+                print(name, str(ver), sep='-')
         else:
-            print('Failed to parse: {}'.format(package), file=sys.stderr)
-    packages = [(name, versions) for name, versions in packages.items()]
-    for name, versions in packages:
-        versions.sort()
-    packages.sort()
-    return packages
+            print(name, end=' ')
+            for ver in versions[:-1]:
+                print(str(ver), end=version_separator)
+            print(versions[-1])
 
-def display_packages(packages, version_separator):
-    for name, versions in packages:
-        print(name, end=' ')
-        for ver in versions[:-1]:
-            print(str(ver), end=version_separator)
-        print(versions[-1])
 
-def validate_args(args, args_parser):
+def old(packages):
+    """Convert a collated list of packages into a list of only old packages."""
+    old_packages = []
+    for name, versions in packages:
+        if len(versions) > 1:
+            old_packages.append((name, versions[:-1]))
+    return old_packages
+
+
+def filter_packages(packages, names):
+    """Filter the packages to only those ones contained in names."""
+    names = set(names)
+    return [(pkg, ver) for pkg, ver in packages if pkg in names]
+
+
+def validate_args(args):
     """Validate that no mutually exclusive arguments have been passed.
 
     This is needed since argparse.ArgumentParser won't let you put mutually
@@ -60,18 +93,22 @@ def validate_args(args, args_parser):
         print("You can't use --version-separator with --id.", file=sys.stderr)
         exit(1)
 
+
 def main():
     args_parser = build_args()
     args = args_parser.parse_args()
-    packages = get_package_list()
-    validate_args(args, args_parser)
-    if args.list == []:
-        display_packages(packages,
-                         version_separator=args.version_separator)
-    elif args.list:
-        for package in args.list:
-            display_packages(packages[package],
-                             version_separator=args.version_separator)
+    validate_args(args)
+    if args.installed is not None:
+        if args.installed:
+            packages = filter_packages(get_package_list(), args.installed)
+        else:
+            packages = get_package_list()
+
+        if args.old:
+            packages = old(packages)
+
+        display_packages(packages, args.version_separator, args.id)
+
     else:
         args_parser.print_help()
 
