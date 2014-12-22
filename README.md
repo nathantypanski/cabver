828aa389bbf95688f12bc5854bef8a6b4477eece
Nathan Typanski <ntypanski@gmail.com>
2014-12-21 20:14:59 -0500

    working "check for new versions" setup

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Quick script to make parsing the output of [cabal](https://www.haskell.org/cabal/) and [ghc-pkg](https://www.haskell.org/haskellwiki/Ghc-pkg) a bit easier.
 
+## Why
+
+Because `ghc-pkg` does a poor job of formatting its output for use in scripts. I usually have two questions when I first query my GHC package database:
+
 ## Usage
 
 To show installed packages:
@@ -12,22 +16,7 @@ Cabal 1.18.1.3 1.20.0.3
 Decimal 0.4.1
 HTTP 4000.2.17 4000.2.19
 JuicyPixels 3.1.7.1
-List 0.5.1
-QuickCheck 2.7.6
-SHA 1.6.4.1
-X11 1.6.1.2
-aeson 0.7.0.6
-aeson-pretty 0.7.1
-array 0.5.0.0
-asn1-encoding 0.8.1.3
 ...
-warp 3.0.0.8
-x509 1.4.12
-x509-store 1.4.4
-x509-system 1.4.5
-x509-validation 1.5.0
-xhtml 3000.2.1
-xml 1.3.13
 yaml 0.8.8.4
 zip-archive 0.2.3.2 0.2.3.4
 zlib 0.5.4.1
@@ -48,29 +37,68 @@ pandoc 1.13.1
 unix 2.7.0.1
 ```
 
-Passing `--old` will list only "old" packages, i.e., packages for which a newer version is currently registered.
+Passing `--multiple` or `-m` will list only packages for which a newer version is currently installed:
 
 ```
-./cabver.py -i --old
-Cabal 1.18.1.3
-HTTP 4000.2.17
-network 2.4.2.3
-texmath 0.6.6.3
-zip-archive 0.2.3.2
+$ ./cabver.py -m -i
+Cabal: 1.18.1.3 1.20.0.3
+HTTP: 4000.2.17 4000.2.19
+network: 2.4.2.3 2.6.0.2
+texmath: 0.6.6.3 0.8
+zip-archive: 0.2.3.2 0.2.3.4
 ```
 
 This may be combined with the other switches as needed:
 
 ```
-$ ./cabver.py -i zip-archive Cabal --old
-Cabal 1.18.1.3
-zip-archive 0.2.3.2
+$ ./cabver.py -m -i zip-archive Cabal
+Cabal: 1.18.1.3 1.20.0.3
+zip-archive: 0.2.3.2 0.2.3.4
 ```
 
 Providing the `--id` argument gives nice IDs for use with `ghc-pkg` or `cabal-install`:
 
 ```
-$ ./cabver.py -i zip-archive Cabal --old --id
+$ ./cabver.py -m -i --id
 Cabal-1.18.1.3
+Cabal-1.20.0.3
+HTTP-4000.2.17
+HTTP-4000.2.19
+network-2.4.2.3
+network-2.6.0.2
+texmath-0.6.6.3
+texmath-0.8
 zip-archive-0.2.3.2
+zip-archive-0.2.3.4
+```
+
+To check for updates to all packages, use `--new-versions` (`-n`):
+
+```
+$ ./cabver.py -n | head -n 5
+Decimal: 0.4.1 -> 0.4.2
+JuicyPixels: 3.1.7.1 -> 3.2
+aeson: 0.7.0.6 -> 0.8.0.2
+aeson-pretty: 0.7.1 -> 0.7.2
+asn1-encoding: 0.8.1.3 -> 0.9.0
+```
+
+When run with `--id`, `-n` will list the IDs of the new versions:
+
+```
+$ ./cabver.py -n --id | head -n 5
+Decimal-0.4.2
+JuicyPixels-3.2
+aeson-0.8.0.2
+aeson-pretty-0.7.2
+asn1-encoding-0.9.0
+```
+
+Which you can then pass into `cabal install` using `xargs`, if you so desire:
+
+```
+./cabver.py -n --id | head -n 2 | xargs cabal install
+Resolving dependencies...
+Configuring Decimal-0.4.2...
+^C
 ```
--- cabver.py
+++ cabver.py
@@ -2,7 +2,6 @@
 
 import argparse
 from distutils.version import LooseVersion
-import collections
 import subprocess
 import sys
 import re
@@ -14,11 +13,8 @@ def build_args():
                       help='list installed versions',
                       type=str,
                       nargs='*')
-    args.add_argument('-o', '--old',
-                      help='Operate only on old versions of packages.'
-                           ' for example, when used with --installed,'
-                           ' this will list only old versions of installed'
-                           ' packages.' ,
+    args.add_argument('-m', '--multiple',
+                      help='Display packages with multiple versions installed.',
                       action='store_true', default=False)
     args.add_argument('-n', '--new-versions', action='store_true')
     display = args.add_argument_group('display options')
@@ -33,25 +29,6 @@ def build_args():
                               ' e.g., Cabal-1.20.0.3',
                          action='store_true', default=False)
     return args
-
-
-def get_package_list():
-    """Get a list of installed packages and their versions from Cabal.
-
-    The output will be a list of the form [[package, version]].
-    """
-    cabal_args = ['cabal', 'list', '--installed', '--simple-output']
-    cabal = subprocess.check_output(cabal_args, universal_newlines=True)
-
-    package_list = [pkg.split(' ') for pkg in cabal.strip('\n').split('\n')]
-    package_map = collections.defaultdict(list)
-    for name, version in package_list:
-        package_map[name].append(LooseVersion(version))
-    package_list = [(name, versions) for name, versions in package_map.items()]
-    package_list.sort(key=lambda x: x[0])
-    for _, versions in package_list:
-        versions.sort()
-    return package_list
 
 
 def _match_package_information(pkg_text):
@@ -75,11 +52,11 @@ def _match_package_information(pkg_text):
                 except AttributeError:
                     pass
     split_and_sort_versions(pkg, 'installed')
-    split_and_sort_versions(pkg, 'available')
+    pkg['available'] = LooseVersion(pkg['available'])
     return pkg
 
 
-def get_available_version_list():
+def get_package_list():
     cabal_args = ['cabal', 'list', '--installed']
     cabal = subprocess.check_output(cabal_args, universal_newlines=True)
     packages = [pkg.split('\n') for pkg in cabal.strip().split('\n\n')]
@@ -89,40 +66,68 @@ def get_available_version_list():
         pkg = _match_package_information(pkg_text)
         package_info_map.append(pkg)
     package_info_map.sort(key=lambda x: x['name'])
-    print(package_info_map)
+    return package_info_map
 
 
-def display_packages(packages, version_separator=None, id_form=False):
+def has_newer_version(pkg):
+    latest = pkg['available']
+    installed = pkg['installed'][-1]
+    try:
+        if installed < latest:
+            return pkg['available']
+    except TypeError as e:
+        # bad version string
+        # (e.g., "Not available from any configured repository")
+        return False
+
+
+def get_new_packages(packages):
+    new_available = []
+    for pkg in packages:
+        if has_newer_version(pkg):
+            new_available.append(pkg)
+    return new_available
+
+
+def display_packages(packages,
+                     version_separator=None,
+                     id_form=False,
+                     show_available=False):
     """Prints to STDOUT a list of package names and their versions.
 
     Args:
         packages: a list [name, [versions]], where versions will be converted
             into strings before printing, and name is a single string.
     """
-    for name, versions in packages:
-        if id_form:
-            for ver in versions:
-                print(name, str(ver), sep='-')
+    if version_separator is None:
+        version_separator = ', '
+    for pkg in packages:
+        if id_form and not show_available:
+            for ver in pkg['installed']:
+                print(pkg['name'], str(ver), sep='-')
+        elif id_form:
+            print(pkg['name'], str(pkg['available']), sep='-')
         else:
-            print(name, end=' ')
-            for ver in versions[:-1]:
-                print(str(ver), end=version_separator)
-            print(versions[-1])
+            installed = version_separator.join(
+                [str(v) for v in pkg['installed']]
+            )
+            if show_available:
+                print('{}: {} -> {}'.format(pkg['name'],
+                                            installed,
+                                            pkg['available']))
+            else:
+                print('{}: {}'.format(pkg['name'], installed))
 
 
 def old(packages):
     """Convert a collated list of packages into a list of only old packages."""
-    old_packages = []
-    for name, versions in packages:
-        if len(versions) > 1:
-            old_packages.append((name, versions[:-1]))
-    return old_packages
+    return [pkg for pkg in packages if len(pkg['installed']) > 1]
 
 
 def filter_packages(packages, names):
     """Filter the packages to only those ones contained in names."""
     names = set(names)
-    return [(pkg, ver) for pkg, ver in packages if pkg in names]
+    return [pkg for pkg in packages if pkg['name'] in names]
 
 
 def validate_args(args):
@@ -145,13 +150,13 @@ def main():
     else:
         packages = get_package_list()
 
-    if args.old:
+    if args.multiple:
         packages = old(packages)
 
     if args.new_versions:
-        get_available_version_list()
-    else:
-        display_packages(packages, args.version_separator, args.id)
+        packages = get_new_packages(packages)
+
+    display_packages(packages, args.version_separator, args.id, args.new_versions)
 
     # args_parser.print_help()
 
